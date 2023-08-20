51a94c7c364fb20726174313c016d55647c00578
Sergey Bulgakov <sergeybulgakov93@mail.ru>
2023-08-20 16:46:24 -0400

    almost everything

--- MainApp/models.py
+++ MainApp/models.py
@@ -2,12 +2,18 @@ from typing import Any
 from django.db import models
 
 # Create your models here.
+class Color(models.Model):
+    name = models.CharField(max_length = 15)
+
+    def __repr__(self):
+        return f'Color {self.name}'
 
 class Item(models.Model):
     name  = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveIntegerField()
     description = models.TextField(max_length=200, default="Temporary not filled")
+    color = models.ManyToManyField(to=Color)
 
     def __repr__(self) -> str:
         return f'Item({self.name} {self.brand} {self.count})'
